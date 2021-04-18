d27f19cd7720fcd59c9c1e1047167acaad3b4b33
l廖汉林 <1491140482@qq.com>
2021-04-18 13:59:51 +0800

    修复excel导出功能

---
+++ Springboot-Excel/src/main/java/com/xd/excel/SpringbootExcelApplication.java
@@ -0,0 +1,16 @@
+package com.xd.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sueno
+ */
+@SpringBootApplication
+public class SpringbootExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExcelApplication.class, args);
+    }
+
+}
---
+++ Springboot-Excel/src/test/java/com/xd/excel/SpringbootExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xd.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
